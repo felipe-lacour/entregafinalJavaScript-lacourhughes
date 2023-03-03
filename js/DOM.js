4156e15492de0c4894a3262f4c850a45accf828f
felipe-lacourhughes <felipelacourhughes@gmail.com>
2023-03-02 21:15:00 -0300

    canasta de compra terminada

--- js/DOM.js
+++ js/DOM.js
@@ -1,5 +1,10 @@
 const mainSeleccion = document.querySelector('#main-seleccion');
 const filtros = document.forms['filtros'];
+const headerIcono = document.querySelector('#header-icono')
+const cartNumero = document.querySelector('#cart-numero')
+const canastaVisualizar = document.querySelector('.visualizar')
+const divTotal = document.querySelector('.total')
+let visualizado = false;
 
 function armadorCarteleras (i){
   const cartelera = document.createElement('div');
@@ -21,19 +26,7 @@ function armadorCarteleras (i){
   `
   mainSeleccion.append(cartelera)
 
-  const botones = document.querySelectorAll('button');
-  botones.forEach((i)=>{
-    i.addEventListener('click', (e)=>{
-      const botonId = e.target.getAttribute('id')
-      pelicula = peliculas.find((i)=>{return i.nombre === botonId})
-      let existe = canastaDeCompra.includes(pelicula)
-      if (!existe){
-        canastaDeCompra.push(pelicula)
-        console.log(canastaDeCompra)
-        return canastaDeCompra;
-      }
-    })
-  })
+  botones();
 }
 
 
@@ -56,6 +49,83 @@ filtros.addEventListener('click', (e) => {
   }
 });
 
+function botones(){
+  const botones = document.querySelectorAll('button');
+  botones.forEach((i)=>{
+    i.addEventListener('click', (e)=>{
+      const botonId = e.target.getAttribute('id')
+      pelicula = peliculas.find((i)=>{return i.nombre === botonId})
+      let existe = canastaDeCompra.includes(pelicula)
+      if (!existe){
+        canastaDeCompra.push(pelicula)
+        canastaNotificacion();
+        divTotal.innerHTML='';        
+        canastaVisualizar.innerHTML = '';
+        canastaFinal = 0
+        canastaDeCompra.forEach(i => armadorCanasta(i));
+        armadorTotal();
+      }
+    })
+  })
+}
+function canastaNotificacion (){
+  if (canastaDeCompra.length >= 1){
+    cartNumero.classList.add('cart-notif_background')
+    cartNumero.innerHTML = `<p>${canastaDeCompra.length}</p>`
+  }
+}
+function canastaClick(){
+  headerIcono.addEventListener('click', () =>{
+    if(canastaDeCompra.length >= 1){
+      if (visualizado === false){
+        visualizado = true;
+        divTotal.style.height = 'fit-content'
+        divTotal.style.borderWidth = '1px'
+      } else {
+        visualizado = false;
+        divTotal.style.height = '0'
+        divTotal.style.borderWidth = '0px'
+      }      
+    }
+
+  })
+}
+
+function armadorCanasta (i){
+  canastaFinal += i.precio
+  const visualizarPelicula = document.createElement('div');
+  visualizarPelicula.classList.add('visualizar-pelicula');
+  visualizarPelicula.innerHTML =`
+  <div class="visualizar-pelicula_poster">
+    <img src="${i.imagen}" alt="${i.nombre}" />
+  </div>
+  <div class="visualizar-pelicula_info">
+    <div class="visualizar-pelicula_titulo">${i.nombre}</div>
+    <div class="visualizar-pelicula_genero">${i.genero.nombre}</div>          
+    <div class="visualizar-pelicula_info__extra">
+      <div class="visualizar-pelicula_fecha">${i.fecha}</div>
+      <div class="visualizar-pelicula_precio">$ ${i.precio}</div>          
+    </div>
+  </div>
+  `
+  canastaVisualizar.append(visualizarPelicula);
+  divTotal.append(canastaVisualizar)
+}
+
+function armadorTotal(){
+  const total = document.createElement('div')
+  total.classList.add('total-precio')
+  total.innerHTML = `
+  <div id="total-canasta"> 
+    <div><h5>TOTAL:</h5></div>
+    <div><p>$${canastaFinal.toFixed(2)}</p></div>
+  </div>
+  `
+
+  divTotal.append(total)
+}
+
 
 
 peliculas.forEach(i => armadorCarteleras(i))
+canastaClick();
