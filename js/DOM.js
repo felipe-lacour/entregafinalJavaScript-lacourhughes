ce251387865aba524d5088af01920a03f9bb95b6
felipe-lacourhughes <felipelacourhughes@gmail.com>
2023-03-04 01:39:10 -0300

    terminado programa busqueda

--- js/DOM.js
+++ js/DOM.js
@@ -6,8 +6,8 @@ const cartNumero = document.querySelector('#cart-numero')
 const canastaVisualizar = document.querySelector('.visualizar')
 const divTotal = document.querySelector('.total')
 let visualizado = false;
-
-function armadorCarteleras (i){
+const resultadosBusqueda = document.querySelector('#resultados-busqueda')
+const armadorCarteleras = (i) => {
   const cartelera = document.createElement('div');
   cartelera.classList.add('cartelera');
   cartelera.innerHTML =`
@@ -29,8 +29,41 @@ function armadorCarteleras (i){
 
   botones();
 }
+const armadorCanasta = (i) => {
+  canastaFinal += i.precio
+  const visualizarPelicula = document.createElement('div');
+  visualizarPelicula.classList.add('visualizar-pelicula');
+  visualizarPelicula.innerHTML =`
+  <div class="visualizar-pelicula_poster">
+    <img src="${i.imagen}" alt="${i.nombre}" />
+  </div>
+  <div class="visualizar-pelicula_info">
+    <div class="visualizar-pelicula_titulo">${i.nombre}</div>
+    <div class="visualizar-pelicula_genero">${i.genero.nombre}</div>          
+    <div class="visualizar-pelicula_info__extra">
+      <div class="visualizar-pelicula_fecha">${i.fecha}</div>
+      <div class="visualizar-pelicula_precio">$ ${i.precio}</div>          
+    </div>
+  </div>
+  `
+  canastaVisualizar.append(visualizarPelicula);
+  divTotal.append(canastaVisualizar)
+}
+const armadorTotal = () => {
+  const total = document.createElement('div')
+  total.classList.add('total-precio')
+  total.innerHTML = `
+  <div id="total-canasta"> 
+    <div><h5>TOTAL:</h5></div>
+    <div><p>$${canastaFinal.toFixed(2)}</p></div>
+  </div>
+  <div class="boton-canasta">
+    <button id="boton-checkout" class="boton-checkout">Checkout</button>
+  </div>
+  `
 
-
+  divTotal.append(total)
+}
 filtros.addEventListener('click', (e) => {
   switch (e.target.value) {
     case 'Drama':
@@ -57,13 +90,6 @@ filtros.addEventListener('click', (e) => {
       break;
   }
 });
-
-const filtroPeliculas = (x)=>{
-      mainSeleccion.innerHTML = '';
-      peliculasElegidas = peliculas.filter(i => i.genero === x);
-      peliculasElegidas.forEach(i => armadorCarteleras(i));
-}
-
 function botones(){
   const botones = document.querySelectorAll('button');
   botones.forEach((i)=>{
@@ -83,13 +109,7 @@ function botones(){
     })
   })
 }
-function canastaNotificacion (){
-  if (canastaDeCompra.length >= 1){
-    cartNumero.classList.add('cart-notif_background')
-    cartNumero.innerHTML = `<p>${canastaDeCompra.length}</p>`
-  }
-}
-function canastaClick(){
+const canastaClick = () => {
   headerIcono.addEventListener('click', () =>{
     if(canastaDeCompra.length >= 1){
       if (visualizado === false){
@@ -109,45 +129,53 @@ function canastaClick(){
     divTotal.style.borderWidth = '0px'
   })
 }
+const filtroPeliculas = (x)=>{
+      mainSeleccion.innerHTML = '';
+      peliculasElegidas = peliculas.filter(i => i.genero === x);
+      peliculasElegidas.forEach(i => armadorCarteleras(i));
+}
+const canastaNotificacion = () => {
+  if (canastaDeCompra.length >= 1){
+    cartNumero.classList.add('cart-notif_background')
+    cartNumero.innerHTML = `<p>${canastaDeCompra.length}</p>`
+  }
+}
 
-function armadorCanasta (i){
-  canastaFinal += i.precio
-  const visualizarPelicula = document.createElement('div');
-  visualizarPelicula.classList.add('visualizar-pelicula');
-  visualizarPelicula.innerHTML =`
-  <div class="visualizar-pelicula_poster">
-    <img src="${i.imagen}" alt="${i.nombre}" />
-  </div>
-  <div class="visualizar-pelicula_info">
-    <div class="visualizar-pelicula_titulo">${i.nombre}</div>
-    <div class="visualizar-pelicula_genero">${i.genero.nombre}</div>          
-    <div class="visualizar-pelicula_info__extra">
-      <div class="visualizar-pelicula_fecha">${i.fecha}</div>
-      <div class="visualizar-pelicula_precio">$ ${i.precio}</div>          
+const inputBuscador = document.querySelector('#input-buscador')
+inputBuscador.addEventListener('input', () => {
+const inputValor = inputBuscador.value
+resultadosBusqueda.innerHTML = ''
+if (inputValor !== ''){  
+  let encontrada = peliculas.filter((i) => {
+    let fecha = i.fecha.toString()
+    if (i.nombre.includes(inputValor) || i.genero.nombre.includes(inputValor) || fecha.includes(inputValor)) {
+      return i
+    }
+  })
+  encontrada.forEach(i => {
+    let hr = document.createElement('hr')
+    let busqueda = document.createElement('div')
+    busqueda.classList.add('busqueda');
+    busqueda.innerHTML = `
+    <div class="busqueda-imagen">
+      <img src="${i.imagen}" alt="${i.nombre}">
     </div>
-  </div>
-  `
-  canastaVisualizar.append(visualizarPelicula);
-  divTotal.append(canastaVisualizar)
+    <div class="busqueda-info">
+      <h3 class="busqueda-titulo">${i.nombre}</h3>    
+      <h5>${i.genero.nombre}</h5>
+      <h4>$${i.precio}</h4>
+    </div>
+    <div class="busqueda-carrito"></div>
+    `
+
+    if(encontrada.length === 1){
+      resultadosBusqueda.append(busqueda)
+    } else if (encontrada.length > 1){
+      resultadosBusqueda.append(busqueda, hr)
+    }
+
+  })
 }
-
-function armadorTotal(){
-  const total = document.createElement('div')
-  total.classList.add('total-precio')
-  total.innerHTML = `
-  <div id="total-canasta"> 
-    <div><h5>TOTAL:</h5></div>
-    <div><p>$${canastaFinal.toFixed(2)}</p></div>
-  </div>
-  <div class="boton-canasta">
-    <button id="boton-checkout" class="boton-checkout">Checkout</button>
-  </div>
-  `
-
-  divTotal.append(total)
-}
-
-
-
+})
 peliculas.forEach(i => armadorCarteleras(i))
 canastaClick();
